52b0d6a48e1a46f231740292a2cd53ac34efe9cb
Emil <emilv.kjellin@gmail.com>
2024-03-12 10:20:39 +0100

    Börjat på design på själva huvudrummet där fights ska hända. Har också lagt till en ny character som snart ska kunnas användas

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,29 +8,58 @@ Raylib.SetTargetFPS(60);
 Texture2D Fighter1 = Raylib.LoadTexture(@"C:\Users\emil.kjellin\Documents\Programmering 1\Slutprojekt\Bilder\Theo AIK KUNG.png");
 Fighter1.Height = 300;
 Fighter1.Width = 600;
-
+Texture2D Fighter2 = Raylib.LoadTexture(@"C:\Users\emil.kjellin\Documents\Programmering 1\Slutprojekt\Bilder\Theo AIK KUNGEN (1).png");
+Fighter2.Height = 320;
+Fighter2.Width = 600;
 
 int game = 0;
 string[] characters = {"NormalTheo", "AikTheo", "TyskTheo", "EldenringTheo"};
+characters[0] = "NormalTheo";
+characters[1] = "AikTheo";
+characters[2] = "TyskTheo";
+characters[3] = "EldenringTheo";
+bool gubbe1 = false;
 while (!Raylib.WindowShouldClose())
 {
 
 
 
 Raylib.BeginDrawing();
+    if (game == 2)
+    {
+        Raylib.ClearBackground(Color.White);
+        Raylib.DrawRectangle(700, 800, 300, 100, Color.DarkBlue);
+        Raylib.DrawText("Fight", 750,800,100,Color.White);
+        int fighter1_hp = 100;
+        int fighter2_hp = 100;
+         if (Raylib.GetMouseX() > 200 && Raylib.GetMouseX() < 800 && Raylib.GetMouseY() > 100 && Raylib.GetMouseY() < 430)
+        {
+            if (Raylib.IsMouseButtonDown(MouseButton.Left))
+            {
+            
+            }
+   
+        }
     
+        if (gubbe1 == true)
+        {
+             Raylib.DrawTexture(Fighter1, 300, 400, Color.White);
+        }
+    }
     if (game == 1)
     {
      Raylib.ClearBackground(Color.SkyBlue);
     Raylib.DrawTexture(Fighter1, 200, 100, Color.White);
+    Raylib.DrawTexture(Fighter2, 1100, 90, Color.White);
     Raylib.DrawText("NormieTheo", 200,400,100,Color.White);
-    
+    Raylib.DrawText("Aik Theo",1180,400,100,Color.White);
+
      if (Raylib.GetMouseX() > 200 && Raylib.GetMouseX() < 800 && Raylib.GetMouseY() > 100 && Raylib.GetMouseY() < 430)
     {
         if (Raylib.IsMouseButtonDown(MouseButton.Left))
         {
             game = 2;
-            characters = "NormalTheo";
+           gubbe1 = true; 
         }
    
     }
@@ -46,6 +75,7 @@ if (game == 0)
         if (Raylib.IsMouseButtonDown(MouseButton.Left))
         {
             game = 1;
+           
         }
    
     }
