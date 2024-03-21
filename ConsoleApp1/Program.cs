16da38587e1712dc3aec7eef04b9db546dd0eb33
Emil <emilv.kjellin@gmail.com>
2024-03-21 10:59:19 +0100

    Har gjort en turnbased combat

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -16,8 +16,8 @@ Texture2D backgroundaik = Raylib.LoadTexture(@"C:\Users\emil.kjellin\Documents\P
 backgroundaik.Height = 1920;
 backgroundaik.Width = 900;
 int game = 0;
-int fighter1_hp = 100;
-int fighter2_hp = 800;
+int fighter1_hp = 500;
+int fighter2_hp = 500;
 int fighter1_dm = 10;
 int fighter2_dm = 10;
 string[] characters = {"NormalTheo", "AikTheo", "TyskTheo", "EldenringTheo"};
@@ -28,34 +28,48 @@ characters[3] = "EldenringTheo";
 bool gubbe1 = false;
 bool gubbe2 = false;
 string playername = "";
+Random random = new Random();
 while (!Raylib.WindowShouldClose())
 {
 
- void Damage_Op()
-{
-fighter2_hp -= fighter1_dm;
+    void Damage_Op()
+    {
+    fighter2_hp -= fighter1_dm;
 
 
-}
-void Damage_player()
-{
-
-}
+    }   
+    void Damage_player()
+    {
+    fighter1_hp -= fighter2_dm;
+    }
 
 Raylib.BeginDrawing();
     if (game == 2)
     {
+        int turn = 0;
         int Fight = 0;
         int fighterhp2 = fighter2_hp; 
-        Raylib.DrawRectangle(700, 800, 300, 100, Color.DarkBlue);
+
+        Raylib.DrawRectangle(750, 800, 300, 100, Color.DarkBlue);
           Raylib.ClearBackground(Color.White);
         
-        Raylib.DrawText("Fight", 750,800,100,Color.White);
-         
-         Raylib.DrawRectangle(900, 100, 800, 100, Color.Gray);
-         Raylib.DrawRectangle(910, 105, fighterhp2, 80, Color.Red);
-         Raylib.DrawText($"{fighterhp2}", 920,100,75,Color.White);
-         if (Raylib.GetMouseX() > 700 && Raylib.GetMouseX() < 1000 && Raylib.GetMouseY() > 750 && Raylib.GetMouseY() < 850)
+        Raylib.DrawText("Fight", 775,800,100,Color.White);
+        
+        
+        //Healthbar me
+        Raylib.DrawRectangle(105, 97, 520, 95, Color.Gray);
+        Raylib.DrawRectangle(110, 103, fighter1_hp, 80, Color.Red);
+        Raylib.DrawText($"{fighter1_hp}", 220,100,75,Color.White);
+
+
+        //Healthbar enemy
+         Raylib.DrawRectangle(1205, 97, 520, 95, Color.Gray);
+         Raylib.DrawRectangle(1210, 105, fighterhp2, 80, Color.Red);
+         Raylib.DrawText($"{fighterhp2}", 1220,100,75,Color.White);
+        
+        if (turn == 0 && game == 2)
+    {
+         if (Raylib.GetMouseX() > 750 && Raylib.GetMouseX() < 1050 && Raylib.GetMouseY() > 750 && Raylib.GetMouseY() < 850)
         {
             if (Raylib.IsMouseButtonUp(MouseButton.Left))
             {
@@ -70,25 +84,50 @@ Raylib.BeginDrawing();
              if (Raylib.IsKeyPressed(KeyboardKey.A))
              {
                 Damage_Op();
+                turn = 1;
              }
+    
+
+       
+        }
+    }
+       if (turn == 1 && game == 2)
+       {
+        
+        int moveop = random.Next(1,3);
+        if (moveop == 1)
         {
-           
+            Damage_player();
         }
+        if (moveop == 2)
+        {
+            Raylib.DrawText("Opp missed his attack", 720, 300, 50,Color.White);
+            
         }
+        if (moveop == 3)
+        {
+            Damage_player();
+            
+        }
+       }
         if (gubbe1 == true)
         {
-             Raylib.DrawTexture(Fighter1, 300, 200, Color.White);
+             Raylib.DrawTexture(Fighter1, 75, 200, Color.White);
         }
         if (gubbe2 == true)
         {
-            Raylib.DrawTexture(backgroundaik, 100, 100, Color.White);
-            Raylib.DrawTexture(Fighter2, 300, 200, Color.White);
+            Raylib.DrawTexture(Fighter2, 75, 200, Color.White);
         }
         if (fighter2_hp == 0)
         {
             game = 3;
         }
+        if (fighter1_hp == 0)
+        {
+            game = 3;
+        }
     }
+    
     if (game == 1)
     {
      Raylib.ClearBackground(Color.SkyBlue);
@@ -135,16 +174,33 @@ if (game == 0)
     }
 }    
 
-if (game == 3)
-{
-    if (fighter2_hp == 0)
+    if (game == 3)
     {
-        Raylib.ClearBackground(Color.Black);
-        Raylib.DrawText($"{playername} Vann" , 800, 470, 50, Color.White);
+        Raylib.DrawText("Restart{Enter}" , 100, 950, 50, Color.White);
+        if (fighter2_hp == 0)
+        {
+            Raylib.ClearBackground(Color.Black);
+            Raylib.DrawText($"{playername} Vann" , 800, 470, 50, Color.White);
+        }
+        if (fighter1_hp == 0)
+        {
+            Raylib.ClearBackground(Color.Black);
+            Raylib.DrawText($"{playername} Lost" , 800, 470, 50, Color.White);
+        }
+        if (Raylib.IsKeyPressed(KeyboardKey.Enter))
+        {
+        game = 1;
+        fighter1_hp = 500;
+        fighter2_hp = 500;
+        gubbe1 = false;
+        gubbe2 = false;
+        }
     }
-}
 
+    
 
 
 Raylib.EndDrawing();
+
 }
+
